2ecadffa151103b15fa7fef72060f95405711b21
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2026-04-22 10:22:46 +0530

    Initial commit - ARP SDN Project

---
+++ arp_controller.py
@@ -0,0 +1,97 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import arp
+
+
+class ARPController(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(ARPController, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+    # Switch features (initial setup)
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Table-miss flow entry
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                         ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    # Add flow helper
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+
+        mod = parser.OFPFlowMod(datapath=datapath,
+                               priority=priority,
+                               match=match,
+                               instructions=inst)
+        datapath.send_msg(mod)
+
+    # Packet handling
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        # Ignore LLDP packets
+        if eth.ethertype == 0x88cc:
+            return
+
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        # Learn MAC address
+        self.mac_to_port[dpid][src] = in_port
+
+        # 🔥 ARP Detection
+        arp_pkt = pkt.get_protocol(arp.arp)
+        if arp_pkt:
+            self.logger.info("ARP Packet: %s -> %s",
+                             arp_pkt.src_ip, arp_pkt.dst_ip)
+
+        # Decide output port
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # Install flow rule (only if not flooding)
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            self.add_flow(datapath, 1, match, actions)
+
+        # Send packet out
+        out = parser.OFPPacketOut(datapath=datapath,
+                                 buffer_id=msg.buffer_id,
+                                 in_port=in_port,
+                                 actions=actions,
+                                 data=msg.data)
+        datapath.send_msg(out)
